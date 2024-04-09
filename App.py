02fc536bbc3f720786c991e842e7d37695e59800
Ascen Destination <64668411+SpiritQmar@users.noreply.github.com>
2024-04-09 22:31:32 +0600

    Initial commit

---
+++ App.py
@@ -0,0 +1,41 @@
+﻿import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+from main import X_train, y_train, activities_list
+
+
+
+
+def recommend_activities_for_day(person_type, model, activities_list, day):
+    activities = []
+    max_recommendations = len(person_type)
+
+    np.random.shuffle(activities_list)
+
+    for i in range(max_recommendations):
+        if person_type[i] == 1 and i < len(activities_list):
+            activities.append(f"{activities_list[i]} ({day})")  
+
+    return activities
+
+def main():
+
+    model = DecisionTreeClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+
+    new_person = np.random.rand(1, 10)  
+
+
+    predicted_type = model.predict(new_person)[0]
+
+
+    days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    for day in days:
+        recommended_activities = recommend_activities_for_day(predicted_type, model, activities_list, day)
+        print(f"Рекомендуемые виды деятельности на {day}:")
+        for activity in recommended_activities:
+            print("- " + activity)
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,43 @@
+﻿import numpy as np
+from sklearn.datasets import make_multilabel_classification
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+X, y = make_multilabel_classification(n_samples=1000, n_features=10, n_classes=10,
+                                      n_labels=2, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+def recommend_activities(person_type, model, activities_list):
+    activities = []
+    max_recommendations = len(person_type)
+
+    np.random.shuffle(activities_list)
+
+    for i in range(max_recommendations):
+        if person_type[i] == 1 and i < len(activities_list):
+            activities.append(activities_list[i])
+
+    return activities
+
+
+activities_list = ["Смотреть сериалы", "Учиться", "Заниматься спортом",
+                   "Работать", "Получать повышение", "Развиваться профессионально", "Бухать",
+                   "Играть в игры", "Делать уроки", "Участвовать в школьных мероприятиях", "Отдыхать от суеты",
+                   "Наслаждаться жизнью"]
+
+new_person = X_test[0].reshape(1, -1)
+predicted_type = model.predict(new_person)[0]
+recommended_activities = recommend_activities(predicted_type, model, activities_list)
+print("Рекомендуемые виды деятельности для Студента сегодня:")
+for activity in recommended_activities:
+    print("- " + activity)
